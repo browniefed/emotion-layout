252866e61798ef3a8f8cd1ce4557da55d59eac78
Jason Brown <browniefed@gmail.com>
2018-02-21 17:12:53 -0800

    Fix col shrink

--- src/col.js
+++ src/col.js
@@ -2,14 +2,16 @@ import React from "react";
 import Box from "./box";
 import styled from "react-emotion";
 
-const Column = ({ shrink, ...props}) => {
-  return <Box
-    {...props}
-    flexBasis={props.flexBasis || props.width}
-    maxWidth={props.maxWidth || props.width}
-    flex={props => (shrink ? "1 1 auto" : "1 0 auto")}
-  />
-}
+const Column = ({ shrink, ...props }) => {
+  return (
+    <Box
+      {...props}
+      flexBasis={props.flexBasis || props.width}
+      maxWidth={props.maxWidth || props.width}
+      flex={shrink ? "1 1 auto" : "1 0 auto"}
+    />
+  );
+};
 
 Column.defaultProps = {
   shrink: true,
