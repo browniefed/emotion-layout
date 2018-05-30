f5880f0200c8a33263bc179bf2709471cba6df65
Jason Brown <jbrown@Jasons-MacBook-Pro.local>
2018-05-30 10:46:56 -0700

    Fix

--- src/div.js
+++ src/div.js
@@ -1,10 +1,32 @@
-import { space, display, propTypes, position } from "styled-system";
+import {
+  space,
+  width,
+  color,
+  fontSize,
+  responsiveStyle,
+  util,
+  display,
+  position
+} from "styled-system";
 import cleanElement from "clean-element";
 import styled from "react-emotion";
 
+const getWidth = n => (!util.num(n) || n > 1 ? util.px(n) : n * 100 + "%");
+
+const maxWidth = responsiveStyle({
+  prop: "maxWidth",
+  key: "maxWidths",
+  getter: getWidth
+});
+
+const minWidth = responsiveStyle({
+  prop: "minWidth",
+  key: "minWidths",
+  getter: getWidth
+});
+
 const CleanDiv = cleanElement("div");
 CleanDiv.propTypes = {
-  ...propTypes.space,
   ...width.propTypes,
   ...maxWidth.propTypes,
   ...minWidth.propTypes,
